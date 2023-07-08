091792b85098ed51fff7e53f002e2e33c31d1861
Zhang Kunyi <90741173+ZinniAZKY@users.noreply.github.com>
2023-07-08 18:10:44 +0900

    Update Trip chain.py

--- Trip chain.py
+++ Trip chain.py
@@ -62,10 +62,11 @@ import os
 #     main()
 
 
-NUM_WORKERS = 4
+NUM_POOL = 6
 
 
-# 新增一列activity并根据purpose修改值
+# 生成出行链，原始数据在PTMerged文件夹，生成数据分别在PTActivity和PTChain中，读取的字典在RegionPurposeCode中
+# 需要修改staypoint_codes，activity_codes_file，chain_dir，merged_path，act_path路径。
 def process_activities(group, activity_dict, activity_dict_next):
     # 更新所有非House停留点之前检验连续停留点是否为2。如果发生移动但目的为不明，则可能出现连续的2的奇数倍个停留点但实际为无法判别的移动。如果停留点为奇数则可能为错误。
     count_house = 0
@@ -84,117 +85,119 @@ def process_activities(group, activity_dict, activity_dict_next):
     # 检测非停留点的两个移动是否连续存在并更新停留点
     for activity in set(activity_dict.values()):
         if activity != 'House':
-            non_house_followed_by_different = (group['activity'] == activity) & \
+            check_next_act = (group['activity'] == activity) & \
                                               (group['activity'].shift(-1) != activity) & \
                                               (group['activity'].shift(-1) != 'House')
 
-            if non_house_followed_by_different.any():
+            if check_next_act.any():
                 print(
                     f"Person id {group.iloc[0]['Person id']} has non-House activity '{activity}' followed by a different activity.")
 
-            mask = (group['activity'] == activity) & (group['activity'].shift(-1) == 'House')
-            following_houses = mask.shift(1).fillna(False)
+            act_mask = (group['activity'] == activity) & (group['activity'].shift(-1) == 'House')
+            next_houses = act_mask.shift(1).fillna(False)
 
-            while following_houses.sum() > 0:
-                group.loc[following_houses, 'activity'] = activity_dict_next[activity]
-                following_houses = (group['activity'] == 'House') & following_houses.shift(1).fillna(False)
+            while next_houses.sum() > 0:
+                group.loc[next_houses, 'activity'] = activity_dict_next[activity]
+                next_houses = (group['activity'] == 'House') & next_houses.shift(1).fillna(False)
 
     return group
 
 
-def process_file(file):
-    data = pd.read_csv(file)
-    data['purpose'] = data['purpose'].astype(str)
-    data['activity'] = data['purpose']
-    data.sort_values(['Person id', 'trip id', 'subtrip id', 'date'], inplace=True)
-    data.reset_index(drop=True, inplace=True)
+def reassign_activity(file):
+    merged_data = pd.read_csv(file)
+    merged_data['purpose'] = merged_data['purpose'].astype(str)
+    merged_data['activity'] = merged_data['purpose']
+    merged_data.sort_values(['Person id', 'trip id', 'subtrip id', 'date'], inplace=True)
+    merged_data.reset_index(drop=True, inplace=True)
 
     staypoint_codes = pd.read_csv('/Users/zhangkunyi/Downloads/RegionPurposeCode/PurposeToActivity.csv')
     activity_dict_next = pd.Series(staypoint_codes.activity.values, index=staypoint_codes.purpose).to_dict()
-    base = os.path.basename(file)
-    base = base.replace("PTMerged", "")
-    activity_codes_file = '/Users/zhangkunyi/Downloads/RegionPurposeCode/' + base
+    base_act = os.path.basename(file)
+    base_act = base_act.replace("PTMerged", "")
+    activity_codes_file = '/Users/zhangkunyi/Downloads/RegionPurposeCode/' + base_act
     activity_codes = pd.read_csv(activity_codes_file, dtype={'code': str})
     activity_dict = pd.Series(activity_codes.purpose.values, index=activity_codes.code).to_dict()
 
-    data['activity'] = data['activity'].replace(activity_dict)
-    data = data.groupby('Person id').apply(process_activities, activity_dict, activity_dict_next).reset_index(drop=True)
+    merged_data['activity'] = merged_data['activity'].replace(activity_dict)
+    grouped_data = merged_data.groupby('Person id').apply(process_activities, activity_dict, activity_dict_next).reset_index(drop=True)
+
+    base_act, ext = os.path.splitext(file)
+    base_act = base_act.replace("PTMerged", "")
+    act_dir = os.path.dirname(base_act) + "/PTActivity"
+    os.makedirs(act_dir, exist_ok=True)
+    act_file = act_dir + "/" + os.path.basename(base_act) + "PTActivity" + ext
+    grouped_data.to_csv(act_file, index=False)
+
+
+def generate_trip_chain(file):
+    # Generate trip chain
+    act_data = pd.read_csv(file)
+    act_data['date'] = pd.to_datetime(act_data['date'])
+    act_data.sort_values(['Person id', 'trip id', 'subtrip id'], inplace=True)
+    act_data_copy = act_data.copy()
+    act_data_copy['end_date'] = act_data_copy.groupby(['Person id', 'trip id', 'subtrip id'])['date'].shift(-1)
+    act_data_copy['end_activity'] = act_data_copy.groupby(['Person id', 'trip id', 'subtrip id'])['activity'].shift(-1)
+    act_data_copy = act_data_copy.dropna(subset=['end_date', 'end_activity'])
+
+    # Convert to 15-minute time chain
+    act_data_copy['date'] = pd.to_datetime(act_data_copy['date']).dt.time
+    act_data_copy['end_date'] = pd.to_datetime(act_data_copy['end_date']).dt.time
+    intervals = pd.date_range(start='00:00', end='23:59', freq='15min').time
+    chain_result = pd.DataFrame(index=act_data_copy['Person id'].unique(), columns=intervals)
+
+    for person_id in act_data_copy['Person id'].unique():
+        person_data = act_data_copy[act_data_copy['Person id'] == person_id]
+        act_duration = {interval: [] for interval in intervals}
+
+        for idx, row in person_data.iterrows():
+            for interval in intervals:
+                act_start = interval
+                act_end = (datetime.combine(date.today(), interval) + timedelta(minutes=15)).time() if interval != \
+                                                                                                       intervals[
+                                                                                                           -1] else time(
+                    23, 59, 59)
+
+                if max(row['date'], act_start) < min(row['end_date'], act_end):
+                    act_start_update = max(row['date'], act_start)
+                    act_end_update = min(row['end_date'], act_end)
+                    act_duration_update = datetime.combine(date.today(), act_end_update) - datetime.combine(
+                        date.today(),
+                        act_start_update)
+
+                    act_duration[interval].append((act_duration_update, row['activity']))
+
+        for interval, durations in act_duration.items():
+            if durations:
+                act_duration_max = max(durations, key=lambda x: x[0])[1]
+                chain_result.loc[person_id, interval] = act_duration_max
+            print("finished")
+
+    chain_result.reset_index(inplace=True)
+    chain_result.rename(columns={'index': 'Person id'}, inplace=True)
 
     base, ext = os.path.splitext(file)
-    output_file = base + "ActivityPT" + ext
-    data.to_csv(output_file, index=False)
+    chain_dir = "/Users/zhangkunyi/Downloads/PTFolder/PTChain"
+    os.makedirs(chain_dir, exist_ok=True)
+    chain_file = chain_dir + "/" + os.path.basename(base).replace("PTActivity", "PTChain") + ext
+
+    chain_result.to_csv(chain_file, index=False)
 
 
 def main():
-    path = '/Users/zhangkunyi/Downloads/PTFolder/PTMerged'
-    files = glob.glob(os.path.join(path, '*.csv'))
+    # 转换purpose为activity
+    merged_path = '/Users/zhangkunyi/Downloads/PTFolder/PTMerged'
+    merged_files = glob.glob(os.path.join(merged_path, '*.csv'))
 
-    with Pool(NUM_WORKERS) as p:
-        p.map(process_file, files)
+    with Pool(NUM_POOL) as p:
+        p.map(reassign_activity, merged_files)
+
+    # 生成trip chain
+    act_path = '/Users/zhangkunyi/Downloads/PTFolder/PTActivity'
+    act_files = glob.glob(os.path.join(act_path, '*.csv'))
+
+    with Pool(NUM_POOL) as p:
+        p.map(generate_trip_chain, act_files)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-# # Define the path for the input CSV files
-# path = '/Users/zhangkunyi/Downloads/PTFolder/PTMerged'
-#
-# # Get a list of all CSV files in the specified directory
-# files = glob.glob(os.path.join(path, '*.csv'))
-#
-# # Process all files
-# for file in files:
-#     process_file(file)
-
-
-
-
-# # 生成 trip chain
-# data = pd.read_csv('/Users/zhangkunyi/Downloads/ShizuokaActivityPT.csv')
-# data['date'] = pd.to_datetime(data['date'])
-# data.sort_values(['Person id', 'trip id', 'subtrip id'], inplace=True)
-# data_copy = data.copy()
-# data_copy['end_date'] = data_copy.groupby(['Person id', 'trip id', 'subtrip id'])['date'].shift(-1)
-# data_copy['end_activity'] = data_copy.groupby(['Person id', 'trip id', 'subtrip id'])['activity'].shift(-1)
-# data_copy = data_copy.dropna(subset=['end_date', 'end_activity'])
-#
-# # 转换为15分钟时间链
-# data_copy['date'] = pd.to_datetime(data_copy['date']).dt.time
-# data_copy['end_date'] = pd.to_datetime(data_copy['end_date']).dt.time
-# intervals = pd.date_range(start='00:00', end='23:59', freq='15min').time
-# result = pd.DataFrame(index=data_copy['Person id'].unique(), columns=intervals)
-#
-# for person_id in data_copy['Person id'].unique():
-#     person_data = data_copy[data_copy['Person id'] == person_id]
-#     activity_duration = {interval: [] for interval in intervals}
-#
-#     for idx, row in person_data.iterrows():
-#         for interval in intervals:
-#             interval_start = interval
-#             interval_end = (datetime.combine(date.today(), interval) + timedelta(minutes=15)).time() if interval != \
-#                                                                                                         intervals[
-#                                                                                                             -1] else time(
-#                 23, 59, 59)
-#
-#             if max(row['date'], interval_start) < min(row['end_date'], interval_end):
-#                 overlap_start = max(row['date'], interval_start)
-#                 overlap_end = min(row['end_date'], interval_end)
-#                 overlap_duration = datetime.combine(date.today(), overlap_end) - datetime.combine(date.today(),
-#                                                                                                   overlap_start)
-#
-#                 activity_duration[interval].append((overlap_duration, row['activity']))
-#
-#     for interval, durations in activity_duration.items():
-#         if durations:
-#             max_duration_activity = max(durations, key=lambda x: x[0])[1]
-#             result.loc[person_id, interval] = max_duration_activity
-#             print("finished")
-#
-#
-# result.reset_index(inplace=True)
-# result.rename(columns={'index': 'Person id'}, inplace=True)
-# result.to_csv('/Users/zhangkunyi/Downloads/ShizuokaTransformedPT.csv', index=False)
