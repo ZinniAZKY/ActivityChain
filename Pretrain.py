6b1a42a482a03abcb61be68ad8c963e209432656
Zhang Kunyi <90741173+ZinniAZKY@users.noreply.github.com>
2023-09-05 09:06:06 +0000

    Update Pretrain.py

--- Pretrain.py
+++ Pretrain.py
@@ -6,9 +6,8 @@ import numpy as np
 from sklearn.metrics import precision_recall_fscore_support
 from transformers import get_linear_schedule_with_warmup
 
-# pretrain用所有城市的数据集，按照0.1分配训练和验证
-train_file = "/home/ubuntu/Documents/TokyoPT/PTChain/combinedhalfTrain.txt"
-val_file = "/home/ubuntu/Documents/TokyoPT/PTChain/combinedhalfEval.txt"
+train_file = "/home/ubuntu/Documents/TokyoPT/PTChain/combinedAttrTrain.txt"
+val_file = "/home/ubuntu/Documents/TokyoPT/PTChain/combinedAttrEval.txt"
 
 with open(train_file, "r", encoding="utf-8") as f:
     train_texts = [line.strip() for line in f.readlines()]
@@ -16,28 +15,24 @@ with open(train_file, "r", encoding="utf-8") as f:
 with open(val_file, "r", encoding="utf-8") as f:
     val_texts = [line.strip() for line in f.readlines()]
 
-# 生成的tokenizer位置
-tokenizer = PreTrainedTokenizerFast(tokenizer_file="/home/ubuntu/Documents/Tokenizer/trip_chain_tokenizer.json")
+tokenizer = PreTrainedTokenizerFast(tokenizer_file="/home/ubuntu/Documents/Tokenizer/trip_chain_tokenizer_attr.json")
 tokenizer.pad_token = "[PAD]"
 tokenizer.eos_token = "[EOS]"
 
-train_encodings = tokenizer(train_texts, padding=True, truncation=True, max_length=48)
-val_encodings = tokenizer(val_texts, padding=True, truncation=True, max_length=48)
+train_encodings = tokenizer(train_texts, padding=True, truncation=True, max_length=51)
+val_encodings = tokenizer(val_texts, padding=True, truncation=True, max_length=51)
 
 # Convert tokenized encodings to torch tensors
 train_input_ids = torch.tensor(train_encodings.input_ids)
-train_labels = torch.tensor(train_encodings.input_ids)  # Labels are the same as inputs for language modeling tasks
-
+train_labels = torch.tensor(train_encodings.input_ids)  # Labels are the same as IDs for language modeling tasks
 val_input_ids = torch.tensor(val_encodings.input_ids)
 val_labels = torch.tensor(val_encodings.input_ids)
 
-# 256-1024
-batch_size = 512
+batch_size = 1024
 train_dataset = TensorDataset(train_input_ids, train_labels)
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
 val_dataset = TensorDataset(val_input_ids, val_labels)
-val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
 
 def calculate_metrics(preds, labels):
@@ -64,32 +59,31 @@ def evaluate_model(model, dataloader, loss_fn):
             total_loss += loss.item()
             num_samples += inputs.size(0)
 
-    average_loss = total_loss / num_samples
+    average_loss = total_loss / len(dataloader)
     return average_loss
 
 
 model = CustomGPT1Model()
 model.initialize_weights()
+tokenizer_vocab_size = len(tokenizer.get_vocab())
+assert model.vocab_size == tokenizer_vocab_size, f"Model's vocab size {model.vocab_size} doesn't match tokenizer's vocab size {tokenizer_vocab_size}"
 
 loss_fn = torch.nn.CrossEntropyLoss()
-# 0.00003，0.00005
-learning_rate = 0.00001
-# weight decay可选
+learning_rate = 0.0001
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
-# 20或15个epochs
-num_epochs = 20
+num_epochs = 10
 num_train_steps = len(train_dataloader) * num_epochs
 scheduler = get_linear_schedule_with_warmup(
     optimizer,
-    num_warmup_steps=0.1 * num_train_steps,
+    num_warmup_steps=0.01 * num_train_steps,
     num_training_steps=num_train_steps
 )
 
 for epoch in range(num_epochs):
     model.train()
-    total_loss = 0.0
-
-    all_preds, all_labels = [], []
+    total_preds, total_labels = [], []
+    total_loss = 0.00
+    total_correct_preds = 0
 
     for step, batch in enumerate(train_dataloader):
         inputs, labels = batch
@@ -105,20 +99,30 @@ for epoch in range(num_epochs):
         preds = np.argmax(logits.detach().cpu().numpy(), axis=-1).flatten()
         labels = labels.detach().cpu().numpy().flatten()
 
-        all_preds.extend(preds)
-        all_labels.extend(labels)
+        correct_preds = (preds == labels).sum()
+        total_correct_preds += correct_preds
+        total_preds.extend(preds)
+        total_labels.extend(labels)
 
-        if step % 500 == 0:
+        if step % 100 == 0:
             print(f"Epoch {epoch + 1}/{num_epochs} | Step {step}/{len(train_dataloader)} | Loss: {loss.item():.8f}")
 
-    average_loss = total_loss / len(train_dataloader)
-    precision, recall, f1, _ = precision_recall_fscore_support(all_labels, all_preds, average='macro', zero_division=0)
-    print(f"Epoch {epoch + 1}/{num_epochs} | Average Loss: {average_loss:.8f} | "
-          f"Precision: {precision:.8f} | Recall: {recall:.8f} | F1: {f1:.8f} | Perplexity: {calculate_perplexity(average_loss):.8f}")
+    average_train_loss = total_loss / len(train_dataloader)
+    accuracy = total_correct_preds / len(total_labels)
+    precision, recall, f1, _ = precision_recall_fscore_support(total_labels, total_preds, average='macro',
+                                                               zero_division=0)
+
+    print(f"Epoch {epoch + 1}/{num_epochs} | "
+          f"Average Loss: {average_train_loss:.8f} | "
+          f"Accuracy: {accuracy:.8f} | "
+          f"Precision: {precision:.8f} | "
+          f"Recall: {recall:.8f} | "
+          f"F1: {f1:.8f} | "
+          f"Perplexity: {calculate_perplexity(average_train_loss):.8f}")
 
     val_loss = evaluate_model(model, val_dataloader, loss_fn)
     print(
-        f"Epoch {epoch + 1}/{num_epochs} | Validation Loss: {val_loss:.8f} | Validation Perplexity: {calculate_perplexity(val_loss):.8f}")
+        f"Epoch {epoch + 1}/{num_epochs} | Validation Loss: {val_loss:.8f} | "
+        f"Validation Perplexity: {calculate_perplexity(val_loss):.8f}")
 
-    torch.save(model, f'/home/ubuntu/Documents/model_{epoch+1}.pth')
-    
+    torch.save(model, f'/home/ubuntu/Documents/model_{epoch + 1}.pth')
