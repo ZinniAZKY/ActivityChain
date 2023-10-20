d33c6d033dfa86e029f46e66111087f4eaa7e44e
Zhang Kunyi <90741173+ZinniAZKY@users.noreply.github.com>
2023-10-20 03:06:10 +0000

    Update Pretrain.py

--- Pretrain.py
+++ Pretrain.py
@@ -1,179 +1,3 @@
-# from CustomGPT1 import CustomGPT1Model
-# import torch
-# from transformers import PreTrainedTokenizerFast
-# from torch.utils.data import DataLoader, TensorDataset
-# import numpy as np
-# from sklearn.metrics import precision_recall_fscore_support
-# from transformers import get_linear_schedule_with_warmup
-# from collections import Counter
-
-# train_file = "/home/ubuntu/Documents/TokyoPT/PTChain/combinedhalfTrain.txt"
-# val_file = "/home/ubuntu/Documents/TokyoPT/PTChain/combinedhalfEval.txt"
-
-# with open(train_file, "r", encoding="utf-8") as f:
-#     train_texts = [line.strip() for line in f.readlines()]
-
-# with open(val_file, "r", encoding="utf-8") as f:
-#     val_texts = [line.strip() for line in f.readlines()]
-
-# tokenizer = PreTrainedTokenizerFast(tokenizer_file="/home/ubuntu/Documents/Tokenizer/trip_chain_tokenizer.json")
-# tokenizer.pad_token = "[PAD]"
-# tokenizer.eos_token = "[EOS]"
-# PAD_TOKEN_ID = tokenizer.convert_tokens_to_ids("[PAD]")
-
-# train_encodings = tokenizer(train_texts, padding=True, truncation=True, max_length=48)
-# val_encodings = tokenizer(val_texts, padding=True, truncation=True, max_length=48)
-
-
-# def shift_input_target(text_encodings):
-#     input_sequences = []
-#     target_sequences = []
-
-#     for seq in text_encodings.input_ids:
-#         input_sequences.extend([seq[:i] for i in range(36, len(seq))])
-#         target_sequences.extend(seq[i] for i in range(36, len(seq)))
-
-#     input_sequences = torch.nn.utils.rnn.pad_sequence([torch.tensor(seq) for seq in input_sequences], batch_first=True, padding_value=PAD_TOKEN_ID)
-
-#     return input_sequences, torch.tensor(target_sequences)
-
-
-# train_input_ids, train_labels = shift_input_target(train_encodings)
-# val_input_ids, val_labels = shift_input_target(val_encodings)
-
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# # # Now calculate class weights
-# # label_freqs = Counter(train_labels.tolist())
-# # max_freq = max(label_freqs.values())
-# # class_weights = {label: max_freq / count for label, count in label_freqs.items()}
-# #
-# # num_classes = 33
-# # weights = [class_weights.get(i, 1.0) for i in range(num_classes)]
-# # weights_tensor = torch.tensor(weights).to(device)
-
-
-# batch_size = 1024
-# train_dataset = TensorDataset(train_input_ids, train_labels)
-# train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-# val_dataset = TensorDataset(val_input_ids, val_labels)
-# val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
-
-
-# def calculate_metrics(preds, labels):
-#     preds = np.argmax(preds, axis=-1).flatten()
-#     labels = labels.flatten()
-#     precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='macro', zero_division=0)
-#     return precision, recall, f1
-
-
-# def calculate_perplexity(loss):
-#     return np.exp(loss)
-
-
-# def evaluate_model(model, dataloader, loss_fn):
-#     model.eval()
-#     total_loss = 0.0
-#     predictions = []
-
-#     with torch.no_grad():
-#         for batch in dataloader:
-#             inputs, labels = batch
-#             inputs, labels = inputs.to(device), labels.to(device)
-#             attention_mask = (inputs != PAD_TOKEN_ID).int().transpose(0, 1)
-#             logits = model(inputs, attention_mask=attention_mask)
-#             loss = loss_fn(logits[:, -1, :], labels)
-#             total_loss += loss.item()
-#             preds = np.argmax(logits[:, -1, :].detach().cpu().numpy(), axis=-1).flatten()
-#             predictions.extend(preds)
-
-#     average_loss = total_loss / len(dataloader)
-#     return average_loss, predictions
-
-
-# model = CustomGPT1Model()
-# model.to(device)
-# model.initialize_weights()
-# tokenizer_vocab_size = len(tokenizer.get_vocab())
-
-# loss_fn = torch.nn.CrossEntropyLoss()
-# # loss_fn = torch.nn.CrossEntropyLoss(weight=weights_tensor)
-
-# learning_rate = 0.000005
-# optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.1)
-# num_epochs = 15
-# num_train_steps = len(train_dataloader) * num_epochs
-# scheduler = get_linear_schedule_with_warmup(
-#     optimizer,
-#     num_warmup_steps=0.01 * num_train_steps,
-#     num_training_steps=num_train_steps
-# )
-
-# for epoch in range(num_epochs):
-#     model.train()
-#     total_preds, total_labels = [], []
-#     total_loss = 0.00
-#     total_correct_preds = 0
-
-#     for step, batch in enumerate(train_dataloader):
-#         inputs, labels = batch
-#         inputs, labels = inputs.to(device), labels.to(device)
-#         attention_mask = (inputs != PAD_TOKEN_ID).int().transpose(0, 1)
-#         logits = model(inputs, attention_mask=attention_mask)
-#         loss = loss_fn(logits[:, -1, :], labels)
-#         loss.backward()
-#         optimizer.step()
-#         optimizer.zero_grad()
-#         scheduler.step()
-
-#         total_loss += loss.item()
-
-#         preds = np.argmax(logits[:, -1, :].detach().cpu().numpy(), axis=-1).flatten()
-#         labels = labels.detach().cpu().numpy().flatten()
-#         correct_preds = (preds == labels).sum()
-#         total_correct_preds += correct_preds
-#         total_preds.extend(preds)
-#         total_labels.extend(labels)
-
-#         if step % 250 == 0:
-#             print(f"Epoch {epoch + 1}/{num_epochs} | Step {step}/{len(train_dataloader)} | Loss: {loss.item():.8f}")
-
-#     average_train_loss = total_loss / len(train_dataloader)
-#     accuracy = total_correct_preds / len(total_labels)
-#     precision, recall, f1, _ = precision_recall_fscore_support(total_labels, total_preds, average='macro',
-#                                                                zero_division=0)
-
-#     print(f"Epoch {epoch + 1}/{num_epochs} | "
-#           f"Average Loss: {average_train_loss:.8f} | "
-#           f"Accuracy: {accuracy:.8f} | "
-#           f"Precision: {precision:.8f} | "
-#           f"Recall: {recall:.8f} | "
-#           f"F1: {f1:.8f} | "
-#           f"Perplexity: {calculate_perplexity(average_train_loss):.8f}")
-
-#     val_loss, val_predictions = evaluate_model(model, val_dataloader, loss_fn)
-#     actual_labels = val_labels.tolist()
-#     predicted_tokens = [tokenizer.decode([pid]) for pid in val_predictions]
-#     actual_tokens = [tokenizer.decode([aid]) for aid in actual_labels]
-#     predicted_token_counts = Counter(predicted_tokens)
-#     actual_token_counts = Counter(actual_tokens)
-
-#     print("Token Occurrences in Ground Truth:")
-#     for token, count in actual_token_counts.most_common():
-#         print(f"{token}: {count}")
-
-#     print("\nToken Occurrences in Predictions:")
-#     for token, count in predicted_token_counts.most_common():
-#         print(f"{token}: {count}")
-
-#     print(
-#         f"Epoch {epoch + 1}/{num_epochs} | Validation Loss: {val_loss:.8f} | "
-#         f"Validation Perplexity: {calculate_perplexity(val_loss):.8f}")
-
-#     torch.save(model, f'/home/ubuntu/Documents/model_{epoch + 1}.pth')
-
-
-
 from CustomGPT1 import CustomGPT1Model
 import torch
 from transformers import PreTrainedTokenizerFast, get_linear_schedule_with_warmup
@@ -277,7 +101,7 @@ def evaluate_model(model, dataloader, loss_fn):
                 device), occupation_ids.to(device), gender_ids.to(device)
             attention_mask = (inputs != PAD_TOKEN_ID).int()
 
-            logits = model(inputs, occupation_ids=occupation_ids, gender_ids=gender_ids, attention_mask=attention_mask)
+            logits = model(inputs, occupation_ids=occupation_ids, gender_ids=gender_ids, attention_mask=attention_mask, use_embeds=False)
 
             loss = loss_fn(logits[:, -1, :], labels)
             total_loss += loss.item()
@@ -345,7 +169,7 @@ if __name__ == "__main__":
                 device), occupation_indices.to(device), gender_indices.to(device)
             attention_mask = (inputs != PAD_TOKEN_ID).int()
             logits = model(inputs, occupation_ids=occupation_indices, gender_ids=gender_indices,
-                           attention_mask=attention_mask)
+                           attention_mask=attention_mask, use_embeds=False)
 
             # prevent the model from predicting non-activity tokens.
             logits[:, -1, EOS_TOKEN_ID] = -1e9
